d38ebe4d3e905aba539583db68fab550147b8dcf
Adophilus <uchenna19of@gmail.com>
2024-04-30 16:16:08 +0100

    updated the entrypoint

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,4 +1,10 @@
 #! /usr/bin/env sh
 
 
+curl 'https://github.com/pressly/goose/releases/download/v3.20.0/goose_linux_x86_64' -o goose
+chmod +x ./goose
+
+# migrate -source ./migrations -database $DATABASE_URL up
+./goose -dir ./migrations postgres $DATABASE_URL up
+
 php -S "0.0.0.0:8080"
